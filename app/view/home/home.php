2656a5d03ffdc26bb109dc908bf7b0b4fd1d70a3
Afaandii <fandisantuy140@gmail.com>
2025-09-15 15:43:48 +0700

    init bootstrap offline

--- app/view/home/home.php
+++ app/view/home/home.php
@@ -4,11 +4,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <title>Halaman Home</title>
 </head>
 
 <body>
-  <h1>Hai ini halaman Home</h1>
+  <h1 class="text-danger text-center">Hai ini halaman Home</h1>
+
+  <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
